a8811e698822358d0fc52fb5391380f3b63d4911
Hugo Bulzomi <hugo.bulzomi@etu.unice.fr>
2022-10-24 10:22:08 +0200

    Début clustering des genres

--- readCsv.py
+++ readCsv.py
@@ -2,12 +2,15 @@ import pandas as pd
 import numpy as np
 import re
 
-dfSongs = pd.read_csv('DATA/wasabi_csv/songs.csv')
-dfArtists = pd.read_csv('DATA/wasabi_csv/wasabi_all_artists_3000.csv')
-dfAlbums = pd.read_csv('DATA/wasabi_csv/albums_all_artists_3000.csv')
+dfSongs = pd.read_csv('DATA/wasabi_csv/songs.csv').dropna()
+dfArtists = pd.read_csv('DATA/wasabi_csv/wasabi_all_artists_3000.csv').dropna()
+dfAlbums = pd.read_csv('DATA/wasabi_csv/albums_all_artists_3000.csv').dropna()
+dfSongs.reset_index(drop=True, inplace=True)
+dfArtists.reset_index(drop=True, inplace=True)
+dfSongs.reset_index(drop=True, inplace=True)
 dfArtists.rename(columns={'genres':'genre'}, inplace=True)
 
-#print(df.columns)
+
 #print(df['genre'][0])
 
 def tri_genre_beta() :
@@ -28,14 +31,43 @@ def tri_genre_beta() :
     return genre
 
 def tri_genre(df) : 
+    global genre_set
     for i in range(len(df['genre'])):
-        if type(df['genre'][i])!=float:
-            df['genre'][i] = df['genre'][i].replace("list(", "").replace("list(", "").replace(")","").replace("\"", "").split(", ")
+        l = df['genre'][i].replace("list(", "").replace(")", '').replace('"', '').replace("Dub(", "").split(', ')
+        genre_set |= set(l)
+        #df['genre'][i] = df['genre'][i].replace("list(", "").replace("list(", "").replace(")","").replace("\"", "").split(", ")
             #print(df['genre'][i])
-    print(df['genre'][1001])
+    #print(df['genre'][1001])
+
+def cluster_genre(df):
+    new_column = []
+    for i in range(len(df['genre'])):
+        clustered_genre = set() 
+        l = df['genre'][i].replace("list(", "").replace(")", '').replace('"', '').replace("Dub(", "").split(', ')
+        for genre in l:
+            for cluster in genre_clusters:
+                if cluster in genre.lower():
+                    clustered_genre.add(cluster)
+                    continue
+        new_column.append(list(clustered_genre))
+    df.insert(loc=len(df.columns), column="genre_cluster", value=new_column)
+
+
+alias = {"dubstep":"electronic", "synthpop":"electronic", "drum and bass":"rock"}
+genre_clusters = ["hip hop", "pop rock", "rock", "metal", "electronic", "pop", "country", "blues", "soul"
+    "classical", "techno", "jazz", "punk", "funk", "disco", "reggae", "R&B", "gospel"
+    "dupstep", "rap", "folk", "bossa nova"]
+genre_set = set()
+
 
 if __name__ == '__main__' :
     tri_genre(dfSongs)
     tri_genre(dfArtists)
     tri_genre(dfAlbums)
+    cluster_genre(dfSongs)
+    print(dfSongs["genre"][:20])
+    print(dfSongs["genre_cluster"][:20])
+    print(genre_set)
+
+
 
---
+++ test_python_rds.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+
+songs = pd.read_csv("Wasabi-dataset-03-10-2022/songs.csv")
+albums = pd.read_csv("Wasabi-dataset-03-10-2022/albums_all_artists_3000.csv")
+
+songs_genre = songs[songs['genre'].notnull()]
+songs_no_genre = songs[songs['genre'].isnull()]
+songs_no_genre.reset_index()
+songs_genre.reset_index()
+
+print("Total:", len(songs))
+print("Avec un genre:",len(songs_genre))
+print("Sans genre:",len(songs_no_genre))
+
+to_deduce = len(songs_no_genre)
+
+for index, row in songs_no_genre.iterrows():
+	alb_id = row["id_album"]
+
+	album = albums[albums["_id"]==alb_id]
+	album = album[album['genre'].notnull()]
+
+	if len(album)>0:
+		to_deduce -=1
+
+print("Sans genre ni album genre:", to_deduce)
