c7c98501c44d86596a0d1d6d98ccb88fb2b667dd
DESKTOP-UAD63F6\Eva <evaradu@hotmail.fr>
2022-11-04 16:19:41 +0100

    working on the TreeMap visualisation

--- TreeMap/example.R
+++ TreeMap/example.R
@@ -5,59 +5,134 @@ if (interactive()) {
   library(ECharts2Shiny)
   
   # Prepare sample data for plotting --------------------------
-  dat <- "[{name: 'A',
+  dat <- "[{name: 'Artiste 1',
               value: 6,
               children: [
                   {
-                  name: 'A-1',
+                  name: 'Genre 1',
                   value: 6,
                   children:[
                   {
-                  name: 'A-1-1',
+                  name: 'Genre 1.1',
                   value: 6
                   },
                   {
-                  name: 'A-1-2',
+                  name: 'Genre 1.2',
                   value: 2
                   }
                   ]
                   },
                   {
-                  name: 'A-2',
+                  name: 'Genre 2',
                   value: 3
                   }
               ]
             },
             {
-              name: 'B',
+              name: 'Artiste 2',
               value: 6,
               children: [
-                  {name : 'B-1',
+                  {name : 'Genre 1',
                   value:10
                   },
                   {
-                  name:'B-2',
+                  name:'Genre 2',
                   value:2
                   }
               ]
             },
             {
-              name: 'C',
-              value: 4
+              name: 'Artiste 3',
+              value: 4,
+              children: [
+                  {name : 'Genre 1',
+                  value:10
+                  },
+                  {
+                  name:'Genre 2',
+                  value:2
+                  }
+              ]
+            }]"
+  
+  dat2 <- "[{name: 'Album 1',
+              value: 6,
+              children: [
+                  {
+                  name: 'Genre 1',
+                  value: 6,
+                  children:[
+                  {
+                  name: 'Genre 1.1',
+                  value: 6
+                  },
+                  {
+                  name: 'Genre 1.2',
+                  value: 2
+                  }
+                  ]
+                  },
+                  {
+                  name: 'Genre 2',
+                  value: 3
+                  }
+              ]
+            },
+            {
+              name: 'Album 2',
+              value: 6,
+              children: [
+                  {name : 'Genre 1',
+                  value:10
+                  },
+                  {
+                  name:'Genre 2',
+                  value:2
+                  }
+              ]
+            },
+            {
+              name: 'Album 3',
+              value: 4,
+              children: [
+                  {name : 'Genre 1',
+                  value:10
+                  },
+                  {
+                  name:'Genre 2',
+                  value:2
+                  }
+              ]
             }]"
   
   # Server function -------------------------------------------
   server <- function(input, output) {
     # Call functions from ECharts2Shiny to render charts
-    renderTreeMap(div_id = "test",
-                  data = dat)
+    output$treePlot <- renderTreeMap(div_id = "test", data = dat, leafDepth=1, name="Visu1")
+    
+    observeEvent(input$typeVisu, 
+      {print(input$typeVisu); 
+        if(input$typeVisu=="genreAlbums"){
+        output$treePlot <- renderTreeMap(div_id = "test", data = dat2, leafDepth=1, name="Visu2")}
+        else{
+          output$treePlot <- renderTreeMap(div_id = "test", data = dat, leafDepth=1, name="Visu1")}
+        },  ## handlerExpr
+        
+      ignoreInit = TRUE
+    )
   }
+  
   
   # UI layout -------------------------------------------------
   ui <- fluidPage(
     # We MUST load the ECharts javascript library in advance
     loadEChartsLibrary(),
-    
+    selectInput(inputId="typeVisu", label = h3("Type de visualisation :"),
+                choices = list("Afficher les genres des artistes" = "genreArtistes", 
+                               "Afficher les genres des musiques" = "genreMusiques", 
+                               "Afficher les genres des albums" = "genreAlbums",
+                               "Afficher les artistes les plus populaires d'un genre" = "artistePopulaires",
+                               "Afficher les genres d'un artiste" = "genreUnArtiste"), selected = "genreArtistes"),
     tags$div(id="test", style="width:100%;height:500px;"),
     deliverChart(div_id = "test")
   )
