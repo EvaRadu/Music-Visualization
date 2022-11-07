90610ca88f838e47f7754d88bb0054139599b6dc
Mia Swery <mia.swery@etu.univ-cotedazur.fr>
2022-11-07 11:11:55 +0100

    working on suburst charts

---
+++ Sunburst/test.js
@@ -0,0 +1,49 @@
+d3.csv('test.csv', function(err, rows){
+
+  function unpack(rows, key) {
+
+  return rows.map(function(row) { return row[key]; });
+
+}
+
+
+var data = [
+
+    {
+
+      type: "sunburst",
+
+      maxdepth: 3,
+
+      ids: unpack(rows, '_id'),
+
+      labels: unpack(rows, 'name'),
+
+      parents:unpack(rows, 'genre')
+
+    }
+
+  ];
+
+
+var layout = {
+
+  margin: {l: 0, r: 0, b: 0, t:0},
+
+  sunburstcolorway:[
+
+    "#636efa","#EF553B","#00cc96","#ab63fa","#19d3f3",
+
+    "#e763fa", "#FECB52","#FFA15A","#FF6692","#B6E880"
+
+  ],
+
+  extendsunburstcolorway: true
+
+};
+
+
+
+Plotly.newPlot('myDiv', data, layout, {showSendToCloud: true});
+
+})
